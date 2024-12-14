596d8f32fa7c67c594bf7100e80411d2a7efb9a7
Kukel Attila <kukel.attila@arteries.hu>
2024-12-14 18:46:57 +0100

    feat: Add test endpoint for fcm

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use App\Http\Controllers\Web\FcmTokenController;
 use App\Http\Controllers\Web\HomeController;
 use App\Http\Controllers\Web\TransactionController;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Notifications\TransactionTriggeredNotification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
@@ -12,3 +15,8 @@ Route::get('/', HomeController::class)->name('home');
 Route::get('transactions', TransactionController::class)->name('transactions.index');
 
 Route::post('fcm-tokens', FcmTokenController::class)->name('fcm-tokens.store');
+
+
+Route::get('t', function () {
+    User::all()->each(fn ($user) => $user->notify(new TransactionTriggeredNotification(Transaction::query()->latest()->first())));
+});

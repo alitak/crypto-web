94fe5edc22728b5ffcabf42f6df74fa992357cae
Kukel Attila <kukel.attila@arteries.hu>
2024-12-15 18:35:32 +0100

    feat: design stuff

--- resources/js/Components/TrendingStocks.vue
+++ resources/js/Components/TrendingStocks.vue
@@ -16,43 +16,43 @@ usePoll(10000)
         <template v-for="(item, index) in props.wallets" :key="index">
             <div class="grid grid-cols-4 gap-3 rounded-[5px] border border-stroke p-3 md:p-5 dark:border-strokedark">
                 <!-- Oszlop 1 -->
-                <div class="col-span-1 flex flex-col items-center justify-center">
-                    <div class="h-11.5 w-11.5 flex items-center justify-center rounded-full bg-[#EEF2F8]">
+                <div class="col-span-1 flex flex-col items-center font-medium">
+                    <div class="h-11.5 w-11.5 flex items-center rounded-full bg-[#EEF2F8]">
                         <img :src="`/images/logo/${item.coin}.svg`" :alt="item.coin" class="h-full w-full object-contain"/>
                     </div>
-                    <div class="mt-3 text-center">
-                        <h5 class="font-bold text-black dark:text-white">
+                    <div class="mt-3 text-center text-xs">
+                        <h5 class="font-bold text-base text-black dark:text-white">
                             {{ item.coin }}
                         </h5>
-                        <p class="text-xs font-medium">{{ item.current_value }}₮</p>
-                        <p class="text-xs font-medium mt-3" :class="item.current_pnl >= 0 ? 'text-meta-3' : 'text-red'">
-                            <span v-text="item.current_pnl >= 0 ? '+' : '-'"></span> {{ item.current_pnl }}%
+                        <p>{{ item.current_value }}₮</p>
+                        <p class="mt-3" :class="item.current_pnl >= 0 ? 'text-meta-3' : 'text-red'">
+                            <span v-text="item.current_pnl >= 0 ? '+' : ''"></span> {{ item.current_pnl }}%
                         </p>
-                        <p class="text-xs font-medium" :class="item.current_pnl >= 0 ? 'text-meta-3' : 'text-red'">
-                            <span v-text="item.current_pnl >= 0 ? '+' : '-'"></span> {{ item.profit }}₮
+                        <p :class="item.current_pnl >= 0 ? 'text-meta-3' : 'text-red'">
+                            <span v-text="item.current_pnl >= 0 ? '+' : ''"></span> {{ item.profit }}₮
                         </p>
                     </div>
                 </div>
                 <!-- Oszlop 2 -->
-                <div class="col-span-2 flex flex-col items-center justify-center">
-                    <div class="text-left font-mono">
-                        <p class="text-xs font-medium">avg {{ item.avg_price }}₮</p>
-                        <p class="text-xs font-medium">min {{ item.min_threshold }}₮ | {{ item.min_percent }}%</p>
-                        <p class="text-xs font-medium">cur {{ item.current_price }}₮</p>
-                        <p class="text-xs font-medium">max {{ item.max_threshold }}₮ | {{ item.max_percent }}%</p>
-                        <p class="text-xs font-medium">stock {{ item.stock }}</p>
-                        <p class="text-xs font-medium">cost {{ item.cost }}₮</p>
+                <div class="col-span-2 flex flex-col items-center">
+                    <div class="text-left font-mono text-sm md:text-base">
+                        <p>avg {{ item.avg_price }}₮</p>
+                        <p :class="{'font-bold text-md text-blue-500': item.min_percent < item.max_percent}">min {{ item.min_threshold }}₮ | {{ item.min_percent }}%</p>
+                        <p>cur {{ item.current_price }}₮</p>
+                        <p :class="{'font-bold text-md text-violet-500': item.max_percent < item.min_percent}">max {{ item.max_threshold }}₮ | {{ item.max_percent }}%</p>
+                        <p>stock {{ item.stock }}</p>
+                        <p>cost {{ item.cost }}₮</p>
                     </div>
                 </div>
                 <!-- Oszlop 3 -->
-                <div class="col-span-1 flex flex-col items-center justify-center">
-                    <div class="text-left font-mono">
-                        <p class="text-xs font-medium">c {{ item.package_count }}</p>
-                        <p class="text-xs font-medium">acc {{ item.account }}₮</p>
-                        <p class="text-xs font-medium">cur {{ item.current_price }}₮</p>
-                        <p class="text-xs font-medium">pac {{ item.packet_price }}₮</p>
-                        <p class="text-xs font-medium">min {{ item.min_threshold_percent_value }}%</p>
-                        <p class="text-xs font-medium">max {{ item.max_threshold_percent_value }}%</p>
+                <div class="col-span-1 flex flex-col items-center">
+                    <div class="text-left font-mono text-xs md:text-sm font-medium">
+                        <p>c {{ item.package_count }}</p>
+                        <p>acc {{ item.account }}₮</p>
+                        <p>cur {{ item.current_price }}₮</p>
+                        <p>pac {{ item.packet_price }}₮</p>
+                        <p>min {{ item.min_threshold_percent_value }}%</p>
+                        <p>max {{ item.max_threshold_percent_value }}%</p>
                     </div>
                 </div>
             </div>

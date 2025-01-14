9c7cefe15ee9e80a4f20a6b96d19c64a31db5f14
Kukel Attila <kukel.attila@arteries.hu>
2025-01-14 15:30:30 +0100

    feat: add link to transactions

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import {Head, Link} from "@inertiajs/vue3"
+import {route} from "ziggy-js";
 
 defineProps<{
     title: string
@@ -16,6 +17,7 @@ defineProps<{
 
                 <div class="mb-4 text-center">
                     <Link class="font-bold text-black text-2xl mb-4" :href="'/'">Home</Link>
+                    <Link class="font-bold text-black text-2xl mb-4 ml-10 sm:hidden" :href="`${route('home')}#transactions`">Transactions</Link>
                 </div>
 
                 <div class="mx-auto max-w-screen-2xl px-4 md:px-6 2xl:px-10">

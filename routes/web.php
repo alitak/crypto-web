3d0fd15b0871e083a057f72e6629dc884fca9daf
Kukel Attila <kukel.attila@arteries.hu>
2025-01-18 07:22:45 +0100

    feat: remove unused codes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Web\FcmTokenController;
 use App\Http\Controllers\Web\HomeController;
-use App\Http\Controllers\Web\TransactionController;
 use App\Models\Transaction;
 use App\Models\User;
 use App\Notifications\TransactionTriggeredNotification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
-
-Route::get('transactions', TransactionController::class)->name('transactions.index');
 
 Route::post('fcm-tokens', FcmTokenController::class)->name('fcm-tokens.store');
 
